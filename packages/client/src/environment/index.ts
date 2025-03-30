73eef22c93f912e0e46cb435ace556e5291ff96d
Aliaksandr Skuratovich <50811069+SkuratovichA@users.noreply.github.com>
2025-03-30 15:59:20 +0200

    try adding protocol to backend url (#32)

--- packages/client/src/environment/index.ts
+++ packages/client/src/environment/index.ts
@@ -4,7 +4,7 @@ const env = import.meta.env.VITE_HOST_ENV;
 
 const apiUrl = match(env as 'development')
   .with('development', () => 'http://localhost:4000/graphql')
-  .otherwise(() => 'cvai-env.eba-3vuhhsf7.eu-central-1.elasticbeanstalk.com')
+  .otherwise(() => 'https://cvai-env.eba-3vuhhsf7.eu-central-1.elasticbeanstalk.com')
 
 export const environment = {
   apiUrl,
