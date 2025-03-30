2b5f3499a6b0aece46f6b4b265558a4fb3e3d3a8
Aliaksandr Skuratovich <50811069+SkuratovichA@users.noreply.github.com>
2025-03-30 01:25:20 +0100

    env setup for frontend (#31)

--- packages/client/src/environment/index.ts
+++ packages/client/src/environment/index.ts
@@ -1,12 +1,10 @@
-// import {match} from "ts-pattern";
-//
-// const env = import.meta.env.VITE_HOST_ENV;
-//
-// const uri = match(env as 'development')
-//   .with('development', () => 'http://localhost:4000/graphql')
-//   .exhaustive();
+import {match} from "ts-pattern";
 
-const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
+const env = import.meta.env.VITE_HOST_ENV;
+
+const apiUrl = match(env as 'development')
+  .with('development', () => 'http://localhost:4000/graphql')
+  .otherwise(() => 'cvai-env.eba-3vuhhsf7.eu-central-1.elasticbeanstalk.com')
 
 export const environment = {
   apiUrl,
