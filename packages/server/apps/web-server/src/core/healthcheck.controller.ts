6dbf9c24480e5691b0596648ed0a36b145fa7041
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2025-04-05 19:38:35 +0200

    fix(be): make healthcheck public

--- packages/server/apps/web-server/src/core/healthcheck.controller.ts
+++ packages/server/apps/web-server/src/core/healthcheck.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from "../auth/decorators/public.decorator";
 
+@Public()
 @Controller('healthcheck')
 export class HealthcheckController {
   @Get()
